a9bba861931c76866e3f6d926d1f9a4ba8a48098
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-12-03 07:50:19 +0530

    RMX3031:[RRO]: Show "SuperDart charging" instead of "Charging rapidly"

--- device.mk
+++ device.mk
@@ -29,7 +29,9 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    TelephonyOverlay \
+	SettingsLibOverlay
 
 # Support legacy stocked firmware
 PRODUCT_FULL_TREBLE_OVERRIDE := true
